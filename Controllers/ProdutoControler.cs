79aca05f9c66debcd6386893509993738a22cb6f
Rafael <rafaelzacharkim@outlook.com>
2025-03-31 19:55:41 -0300

    Inicializando repositório

---
+++ Controllers/ProdutoControler.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/produto")]
+public class ProdutoController : ControllerBase
+{
+
+    public string Metodo()
+    {
+        return "hello world ";
+    }
+    //
+    [HttpGet("hello world")]
+}
